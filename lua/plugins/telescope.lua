9a1ffc9d8710e9b7d690bdf87aa6f428247de4ef
Stefan Atanaskovic <stefan.atanaskovic02@gmail.com>
2024-08-17 22:59:33 +0200

    fix clangd (on mac atleast) + remove some telecdope keybind

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -57,15 +57,6 @@ return { -- Fuzzy Finder (files, lsp, etc)
         })
       end, { desc = '[/] Fuzzily search in current buffer' })
 
-      -- It's also possible to pass additional configuration options.
-      --  See `:help telescope.builtin.live_grep()` for information about particular keys
-      vim.keymap.set('n', '<leader>s/', function()
-        builtin.live_grep {
-          grep_open_files = true,
-          prompt_title = 'Live Grep in Open Files',
-        }
-      end, { desc = '[S]earch [/] in Open Files' })
-
       -- Shortcut for searching your Neovim configuration files
       vim.keymap.set('n', '<leader>sn', function()
         builtin.find_files { cwd = vim.fn.stdpath 'config' }
